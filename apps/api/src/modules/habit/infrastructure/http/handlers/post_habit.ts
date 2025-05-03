d1409d65736fe48f7990e30af7f2edd8eb5c838d
LuloDev <134250506+LuloDev@users.noreply.github.com>
2025-05-03 10:37:21 -0500

    refactor(habit): move HTTP handlers to infrastructure layer

--- apps/api/src/modules/habit/infrastructure/http/handlers/post_habit.ts
+++ apps/api/src/modules/habit/infrastructure/http/handlers/post_habit.ts
@@ -5,12 +5,12 @@ import { SqlHabits } from "@habitus/database";
 import { CreateHabitUseCase } from "@habit/application/use_cases/create_habit_use_case";
 
 export const postHabit = async (c: Context) => {
-  const body = await c.req.json();
+	const body = await c.req.json();
 
-  const parsedBody = parse(CreateHabitSchema, body);
-  const sqlHabits = new SqlHabits();
-  const useCase = new CreateHabitUseCase(sqlHabits);
-  const result = await useCase.execute(parsedBody);
+	const parsedBody = parse(CreateHabitSchema, body);
+	const sqlHabits = new SqlHabits();
+	const useCase = new CreateHabitUseCase(sqlHabits);
+	const result = await useCase.execute(parsedBody);
 
-  return c.json(result);
+	return c.json(result);
 };
