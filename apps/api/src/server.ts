7ec3e1c308ff77f65c5e200cff3ccf5db8b2c60c
LuloDev <134250506+LuloDev@users.noreply.github.com>
2025-05-08 21:30:24 -0500

    perf(api): improve CORS configuration

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -3,6 +3,7 @@ import { type Context, Hono } from "hono";
 import { HTTPException } from "hono/http-exception";
 import { logger } from "hono/logger";
 import { compress } from "hono/compress";
+import { cors } from "hono/cors";
 import { habitRoutes } from "@/habit/infrastructure/http/routes";
 import { errorResponse } from "@/core/infrastructure/helpers/api_response";
 import { habitinstanceRoutes } from "@/habit_instance/infrastructure/http/routes";
@@ -11,27 +12,34 @@ const app = new Hono().basePath("/api");
 app.use(logger());
 app.use(compress());
 
+app.use(
+	"*",
+	cors({
+		origin: "*",
+	}),
+);
+
 app.route("/habits", habitRoutes);
 
 app.route("/habits/:habitId/instances", habitinstanceRoutes);
 
 app.get("/health", (c) => {
-  return c.json({ status: "ok" });
+	return c.json({ status: "ok" });
 });
 
 app.onError((err: Error, c: Context) => {
-  if (err instanceof HTTPException) {
-    return c.json(errorResponse(err.message, err.cause), err.status);
-  }
-  return c.json(errorResponse(err.message, err.stack), 500);
+	if (err instanceof HTTPException) {
+		return c.json(errorResponse(err.message, err.cause), err.status);
+	}
+	return c.json(errorResponse(err.message, err.stack), 500);
 });
 
 serve(
-  {
-    fetch: app.fetch,
-    port: 3001,
-  },
-  (info) => {
-    console.log(`Server is running on http://localhost:${info.port}`);
-  },
+	{
+		fetch: app.fetch,
+		port: 3001,
+	},
+	(info) => {
+		console.log(`Server is running on http://localhost:${info.port}`);
+	},
 );
