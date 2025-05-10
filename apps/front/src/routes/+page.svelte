a39f1e33105151803910119b54e0e1ae911fdba8
LuloDev <134250506+LuloDev@users.noreply.github.com>
2025-05-10 12:19:10 -0500

    style(page): Improve habit list layout with flexbox

--- apps/front/src/routes/+page.svelte
+++ apps/front/src/routes/+page.svelte
@@ -1,10 +1,20 @@
 <script lang="ts">
-  import Habit from "$lib/components/Habit.svelte";
-  import type { HabitWithInstancesDto } from "@habitus/validation";
+import Habit from "$lib/components/Habit.svelte";
+import type { HabitWithInstancesDto } from "@habitus/validation";
 
-  export let data: { habits: HabitWithInstancesDto[] };
+export let data: { habits: HabitWithInstancesDto[] };
 </script>
 
-{#each data.habits as habit}
-  <Habit {habit} />
-{/each}
+<main class="habits-list" id="habitsList">
+  {#each data.habits as habit}
+    <Habit {habit} />
+  {/each}
+</main>
+
+<style>
+  .habits-list {
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+  }
+</style>
